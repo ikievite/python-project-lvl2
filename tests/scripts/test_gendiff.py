aef150e2da220b740b311e9469141404cc84cef3
ikievite <ikievite@gmail.com>
2020-12-14 15:02:31 +0200

    add test for diff yaml/yml files

--- tests/scripts/test_gendiff.py
+++ tests/scripts/test_gendiff.py
@@ -4,8 +4,17 @@ from gendiff.scripts.gendiff import generate_diff
 
 
 def test_generate_diff():
-    with open('tests/fixtures/diff_flat_1_2.txt') as f:
+    with open('tests/fixtures/diff_flat_json.txt') as f:
         expected = f.read().strip()
     file1 = 'tests/fixtures/flat1.json'
     file2 = 'tests/fixtures/flat2.json'
     assert generate_diff(file1, file2) == expected
+
+    with open('tests/fixtures/diff_flat_yaml.txt') as f:
+        expected_yaml = f.read().strip()
+    flat1_yaml = 'tests/fixtures/flat1.yaml'
+    flat2_yaml = 'tests/fixtures/flat2.yaml'
+    flat1_yml = 'tests/fixtures/flat1.yml'
+    flat2_yml = 'tests/fixtures/flat2.yml'
+    assert generate_diff(flat1_yaml, flat2_yaml) == expected_yaml
+    assert generate_diff(flat1_yml, flat2_yml) == expected_yaml
