07098b81fef14a20e66e1d08af3d01df7363e810
ikievite <ikievite@gmail.com>
2021-01-23 22:03:40 +0200

    update stylish formater, remove duplication

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -23,6 +23,27 @@ def encode_to_json_type(value):  # noqa: WPS110
     return node_value
 
 
+def diff_line(depth, node):
+    """Func generates line.
+
+    Args:
+        depth: level of indentation
+        node: dict with diff data
+
+    Returns:
+        line
+    """
+    badge = node['badge']
+    key = node['name']
+    if node['type'] == 'flat':
+        value = encode_to_json_type(node['value'])
+    else:
+        value = '{'
+    return '  {indent}{badge} {key}: {value}'.format(
+        indent='  '*depth, badge=badge, key=key, value=value,
+    )
+
+
 def stylish_formater(diff):
     """Func that display diff tree.
 
@@ -39,33 +60,28 @@ def stylish_formater(diff):
         for node in nodes:  # noqa: WPS426
             if node['type'] == 'nested':
                 node['children'].sort(key=lambda child: child['name'])
-                output.append('  {0}{1} {2}: {3}'.format(
-                    '  '*depth, node['badge'], node['name'], '{',
-                ))
+                output.append(diff_line(depth, node))
                 iter_node(node['children'], depth + 2)
 
             elif node['type'] == 'complex':
-                output.append('  {0}{1} {2}: {3}'.format(
-                    '  '*depth, node['badge'], node['name'], '{',
-                ))
+                output.append(diff_line(depth, node))
 
                 def iter_complex(complex_node, depth):  # noqa: WPS430, WPS442
                     for node_key, node_value in complex_node.items():
+                        diff_comlex_line = '      {indent}  {key}: {vaue}'
                         if isinstance(node_value, dict):
-                            output.append('      {0}  {1}: {2}'.format(
-                                '  '*depth, node_key, '{',
+                            output.append(diff_comlex_line.format(
+                                indent='  '*depth, key=node_key, value='{',
                             ))
                             iter_complex(node_value, depth + 2)
                         else:
-                            output.append('      {0}  {1}: {2}'.format(
-                                '  '*depth, node_key, node_value,
+                            output.append(diff_comlex_line.format(
+                                indent='  '*depth, key=node_key, value=node_value,
                             ))
                     output.append('    {0}{1}'.format('  '*depth, '}'))
                 iter_complex(node['value'], depth)
             elif node['type'] == 'flat':
-                output.append('  {0}{1} {2}: {3}'.format(
-                    '  '*depth, node['badge'], node['name'], encode_to_json_type(node['value']),
-                ))
+                output.append(diff_line(depth, node))
         output.append('{0}{1}'.format('  '*depth, '}'))
         return '\n'.join(output)
     return iter_node(diff, 0)
