8b3a15a4e6af77f3fc7f4f33e27a7d1185cc0cec
ikievite <ikievite@gmail.com>
2021-01-23 14:11:58 +0200

    update test's names

--- tests/scripts/test_gendiff.py
+++ tests/scripts/test_gendiff.py
@@ -1,4 +1,7 @@
+
+
 ''' Tests for gendiff project.'''
+
 
 from gendiff.scripts.gendiff import generate_diff
 
@@ -27,7 +30,7 @@ def test_generate_diff_flat_yml():
     assert generate_diff(flat1_yml, flat2_yml) == expected_yml
 
 
-def test_generate_diff_def_stylish():
+def test_generate_diff_flat_stylish():
     with open('tests/fixtures/diff_flat.txt') as f:
         expected = f.read().strip()
     file1 = 'tests/fixtures/flat1.json'
@@ -35,7 +38,7 @@ def test_generate_diff_def_stylish():
     assert generate_diff(file1, file2, 'stylish') == expected
 
 
-def test_generate_diff_nested_json():
+def test_generate_diff_nested_stylish():
     with open('tests/fixtures/diff_nested_stylish.txt') as f:
         expected = f.read().strip()
     file1 = 'tests/fixtures/nested1.json'
@@ -43,12 +46,13 @@ def test_generate_diff_nested_json():
     assert generate_diff(file1, file2) == expected
 
 
-def test_generate_diff_nested_yaml():
+def test_generate_diff_nested_stylish_yaml():
     with open('tests/fixtures/diff_nested_stylish.txt') as f:
         expected = f.read().strip()
     file1 = 'tests/fixtures/nested1.yaml'
     file2 = 'tests/fixtures/nested2.yaml'
     assert generate_diff(file1, file2) == expected
+
 
 def test_generate_diff_nested_plain():
     with open('tests/fixtures/diff_nested_plain.txt') as f:
@@ -64,4 +68,3 @@ def test_generate_diff_nested_json_formater():
     file1 = 'tests/fixtures/nested1.json'
     file2 = 'tests/fixtures/nested2.json'
     assert generate_diff(file1, file2, 'json') == expected
-
