bbf73881a6302246ae7f39f6dcb507b611bd0b7b
ikievite <ikievite@gmail.com>
2021-02-02 14:38:47 +0200

    remove useless parameter "required=False"

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument(
-        '-f', '--format', default='stylish', required=False,  # noqa: WPS317
+        '-f', '--format', default='stylish',  # noqa: WPS317
         dest='formater', help='set output format (default: "stylish")',
     )
     args = parser.parse_args()
