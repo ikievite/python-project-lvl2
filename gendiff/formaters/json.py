0c61890c8c39e46c8c8159c50687ef1bc70bbffa
ikievite <ikievite@gmail.com>
2021-02-15 11:20:42 +0200

    removed duplicate sort

--- gendiff/formaters/json.py
+++ gendiff/formaters/json.py
@@ -15,12 +15,10 @@ def json_formater(diff):
     Returns:
         output in json format
     """
-    diff.sort(key=lambda node: node['name'])
-
     def iter_node(nodes):  # noqa: WPS430
+        nodes.sort(key=lambda entry: entry['name'])
         for node in nodes:  # noqa: WPS426, WPS442
             if node['type'] == 'nested':
-                node['children'].sort(key=lambda child: child['name'])
                 iter_node(node['children'])
         return json.dumps(diff, indent=4)
     return iter_node(diff)
