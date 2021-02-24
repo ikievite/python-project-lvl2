34cd97ac29acce97a4f712cb07f7788860635733
ikievite <ikievite@gmail.com>
2021-02-24 10:20:45 +0200

    added checking exception msg

--- tests/test_format_diff.py
+++ tests/test_format_diff.py
@@ -7,5 +7,7 @@ from gendiff.format_diff import format_diff
 
 
 def test_format_diff_jsonlike_formater():
-    with pytest.raises(Exception):
+    with pytest.raises(Exception) as excinfo:
         format_diff({}, 'jsonlike')
+    exception_msg = excinfo.value.args[0]
+    assert exception_msg == "unsupported formater type 'jsonlike'"
