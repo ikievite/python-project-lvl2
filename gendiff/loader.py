b5023b99c8487e4998ce9fae4c274560234089b1
ikievite <ikievite@gmail.com>
2021-04-13 11:31:47 +0300

    added description for noqa

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -22,11 +22,11 @@ def loader(filename):
     """
     filename = filename.lower()
     try:
-        with open(filename) as f:  # noqa: WPS111
+        with open(filename) as f:  # noqa: WPS111 # ignore too short name
             if 'json' in filename:
                 return json.load(f)
             elif 'yaml' in filename or 'yml' in filename:
                 return yaml.safe_load(f)
             raise Exception('Wrong file type, neither json nor yaml/yml')
-    except Exception:  # noqa: WPS329
+    except Exception:  # noqa: WPS329 # ignore allow `except` case
         raise
