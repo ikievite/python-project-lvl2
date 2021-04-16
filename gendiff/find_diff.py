8599284d3d7eac893dde3da965d8fb8e3e2a2d41
ikievite <ikievite@gmail.com>
2021-04-16 12:43:08 +0300

    removed block variables overlap, string constant over-use: state, node, value

--- gendiff/find_diff.py
+++ gendiff/find_diff.py
@@ -7,6 +7,10 @@ ADDED = 'ADDED'
 REMOVED = 'REMOVED'
 CHANGED = 'CHANGED'
 UNCHANGED = 'UNCHANGED'
+NODE_NAME = 'name'
+NODE_STATE = 'state'
+NODE_VALUE = 'value'
+NODE_CHILDREN = 'children'
 
 
 def find_diff(dict1, dict2):
@@ -21,41 +25,41 @@ def find_diff(dict1, dict2):
     """
     diff = []
 
-    for key in dict2.keys():
+    for key in dict2:
         value1 = dict1.get(key)
         value2 = dict2.get(key)
         if key in dict1.keys():
             if value1 == value2:
                 diff.append({
-                    'name': key,
-                    'state': UNCHANGED,
-                    'value': value2,
+                    NODE_NAME: key,
+                    NODE_STATE: UNCHANGED,
+                    NODE_VALUE: value2,
                 })
             elif isinstance(value1, dict) and isinstance(value2, dict):
                 diff.append({
-                    'name': key,
-                    'state': UNCHANGED,
-                    'children': find_diff(value1, value2),
+                    NODE_NAME: key,
+                    NODE_STATE: UNCHANGED,
+                    NODE_CHILDREN: find_diff(value1, value2),
                 })
             else:
                 diff.append({
-                    'name': key,
-                    'state': CHANGED,
-                    'value': {REMOVED: value1, ADDED: value2},
+                    NODE_NAME: key,
+                    NODE_STATE: CHANGED,
+                    NODE_VALUE: {REMOVED: value1, ADDED: value2},
                 })
         else:
             diff.append({
-                'name': key,
-                'state': ADDED,
-                'value': value2,
+                NODE_NAME: key,
+                NODE_STATE: ADDED,
+                NODE_VALUE: value2,
             })
-    for key in dict1.keys():  # noqa: WPS440 # ignore var overlap
-        value1 = dict1.get(key)
-        if key not in dict2.keys():
+    for node_key in dict1:
+        value1 = dict1.get(node_key)
+        if node_key not in dict2.keys():
             diff.append({
-                'name': key,
-                'state': REMOVED,
-                'value': value1,
+                NODE_NAME: node_key,
+                NODE_STATE: REMOVED,
+                NODE_VALUE: value1,
             })
 
     return diff
