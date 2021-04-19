c406d64a94abafa4ce5f85487b071ca851eb814e
ikievite <ikievite@gmail.com>
2021-04-19 23:30:33 +0300

    used sets to find added and unchanged keys

--- gendiff/find_diff.py
+++ gendiff/find_diff.py
@@ -24,39 +24,37 @@ def find_diff(dict1, dict2):
         list with diff items
     """
     diff = []
-    for key in dict2:
+    for key in set(dict1) & set(dict2):
         value1 = dict1.get(key)
         value2 = dict2.get(key)
-        if key in dict1.keys():
-            if value1 == value2:
-                diff.append({
-                    NODE_NAME: key,
-                    NODE_STATE: UNCHANGED,
-                    NODE_VALUE: value2,
-                })
-            elif isinstance(value1, dict) and isinstance(value2, dict):
-                diff.append({
-                    NODE_NAME: key,
-                    NODE_STATE: UNCHANGED,
-                    NODE_CHILDREN: find_diff(value1, value2),
-                })
-            else:
-                diff.append({
-                    NODE_NAME: key,
-                    NODE_STATE: CHANGED,
-                    NODE_VALUE: {REMOVED: value1, ADDED: value2},
-                })
+        if value1 == value2:
+            diff.append({
+                NODE_NAME: key,
+                NODE_STATE: UNCHANGED,
+                NODE_VALUE: value2,
+            })
+        elif isinstance(value1, dict) and isinstance(value2, dict):
+            diff.append({
+                NODE_NAME: key,
+                NODE_STATE: UNCHANGED,
+                NODE_CHILDREN: find_diff(value1, value2),
+            })
         else:
             diff.append({
                 NODE_NAME: key,
-                NODE_STATE: ADDED,
-                NODE_VALUE: value2,
+                NODE_STATE: CHANGED,
+                NODE_VALUE: {REMOVED: value1, ADDED: value2},
             })
-    for node_key in set(dict1) - set(dict2):
-        removed_value = dict1.get(node_key)
+    for added_key in set(dict2) - set(dict1):
         diff.append({
-            NODE_NAME: node_key,
+            NODE_NAME: added_key,
+            NODE_STATE: ADDED,
+            NODE_VALUE: dict2.get(added_key),
+        })
+    for removed_key in set(dict1) - set(dict2):
+        diff.append({
+            NODE_NAME: removed_key,
             NODE_STATE: REMOVED,
-            NODE_VALUE: removed_value,
+            NODE_VALUE: dict1.get(removed_key),
         })
     return diff
