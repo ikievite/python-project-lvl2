1cb63200be98c10f6ba256c322f12e9ccfecce44
ikievite <ikievite@gmail.com>
2021-04-20 10:47:35 +0300

    added expected exceptions

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,8 @@
 
 from gendiff.args_parser import args_parse
 from gendiff.generate_diff import generate_diff
+from gendiff.loader import FileTypeError
+from gendiff.format_diff import FormaterError
 
 
 def main():
@@ -14,8 +16,10 @@ def main():
         diff = generate_diff(args.first_file, args.second_file, args.formater)
 
         print(diff)  # noqa: WPS421 # allow print call
-    except Exception as e:  # noqa: WPS111 # ignore warning about to short name
+    except FormaterError as e:  # noqa: WPS111 # ignore warning about to short name
         print('Exception: {0}'.format(str(e)))  # noqa: WPS421, ignore warning about print call
+    except FileTypeError as e:  # noqa: WPS111
+        print('Exception: {0}'.format(str(e)))  # noqa: WPS421
 
 
 if __name__ == '__main__':
