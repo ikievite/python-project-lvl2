61de2bbccd45cbb72779454ee3cf5fe36e23b0f1
ikievite <ikievite@gmail.com>
2021-04-21 15:00:48 +0300

    moved test for json formater to separate function

--- tests/scripts/test_gendiff.py
+++ tests/scripts/test_gendiff.py
@@ -2,8 +2,10 @@
 
 ''' Tests for gendiff project.'''
 
+import json
 import pytest
 from gendiff.scripts.gendiff import generate_diff
+from gendiff.find_diff import NODE_NAME
 
 
 test_flat_data = [('tests/fixtures/flat1.json',
@@ -37,10 +39,6 @@ test_data_with_formaters = [('tests/fixtures/nested1.json',
                              'tests/fixtures/nested2.json',
                              'plain',
                              'tests/fixtures/diff_nested_plain.txt'),
-                            #('tests/fixtures/nested1.json',
-                            # 'tests/fixtures/nested2.json',
-                            # 'json',
-                            # 'tests/fixtures/diff_nested_json_formater.txt'),
                             ('tests/fixtures/nested1.json',
                              'tests/fixtures/nested2.json',
                              'stylish',
@@ -52,3 +50,25 @@ def test_generate_diff_with_formater(file1, file2, formater, diff):
     with open(diff) as f:
         expected = f.read().strip()
     assert generate_diff(file1, file2, formater) == expected
+
+
+def convert_to_dict(l):
+    result = {}
+    for node in l:
+        result.update({node[NODE_NAME]:node})
+    return result
+
+
+test_data_for_json_formater = [('tests/fixtures/nested1.json',
+                                'tests/fixtures/nested2.json',
+                                'json',
+                                'tests/fixtures/diff_nested_json_formater.txt')]
+
+
+@pytest.mark.skip(reason='to do')
+@pytest.mark.parametrize("file1,file2,formater,diff", test_data_for_json_formater)
+def test_generate_diff_json_formater(file1, file2, formater, diff):
+    with open(diff) as f:
+        json_diff = json.load(f)
+    expected = convert_to_dict(json_diff)
+    assert convert_to_dict(json.loads(generate_diff(file1, file2, formater))) == expected
