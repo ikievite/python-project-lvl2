454b7f83a464c585f24a831c2eb02593c36ec827
ikievite <ikievite@gmail.com>
2021-04-27 16:48:50 +0300

    updated name for function from `loader` to `load_content`

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,7 +5,7 @@
 
 from gendiff.find_diff import find_diff
 from gendiff.formaters.format_diff import STYLISH_VIEW, format_diff
-from gendiff.loader import loader
+from gendiff.loader import load_content
 
 
 def generate_diff(file1, file2, formater=STYLISH_VIEW):
@@ -19,6 +19,6 @@ def generate_diff(file1, file2, formater=STYLISH_VIEW):
     Returns:
         string with diff
     """
-    content1, content2 = loader(file1), loader(file2)
+    content1, content2 = load_content(file1), load_content(file2)
     diff = find_diff(content1, content2)
     return format_diff(diff, formater)
